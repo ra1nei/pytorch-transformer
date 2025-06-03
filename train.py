d44493d605a5a3916e57126e1a99fb12817d969d
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-06-03 09:32:47 +0700

    Update train.py

--- train.py
+++ train.py
@@ -141,6 +141,7 @@ def get_or_build_tokenizer(config, ds, lang):
 def get_ds(config):
     # It only has the train split, so we divide it overselves
     ds_raw = load_dataset(f"{config['datasource']}", split='train')
+    print(ds_raw)
 
     # Build tokenizers
     tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
